5c23b9cd127c4437670e65b988575598e9e23097
RiP3rQ <rafalpompa2000@gmail.com>
2024-01-16 18:43:15 +0100

    finished AI Chatbot

--- components/AIChatBot/ChatbotBox.tsx
+++ components/AIChatBot/ChatbotBox.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useChat } from "ai/react";
 import { cn } from "@/lib/utils";
-import { XCircle } from "lucide-react";
+import { Bot, Trash, XCircle } from "lucide-react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Message } from "ai";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 type Props = {
   open: boolean;
@@ -21,6 +23,26 @@ const ChatbotBox: React.FC<Props> = ({ open, onClose }) => {
     isLoading,
     error,
   } = useChat();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  // scroll to bottom when new message is being streamed
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  // add focus to input when chatbot is opened
+  useEffect(() => {
+    if (open) {
+      inputRef.current?.focus();
+    }
+  }, [open]);
+
+  const lastMessageIsUser = messages[messages.length - 1]?.role === "user";
+
   return (
     <div
       className={cn(
@@ -32,16 +54,46 @@ const ChatbotBox: React.FC<Props> = ({ open, onClose }) => {
         <XCircle size={30} />
       </button>
       <div className="flex h-[600px] flex-col rounded border bg-background shadow-xl">
-        <div className="h-full">
+        <div className="mt-3 h-full overflow-y-auto px-3" ref={scrollRef}>
           {messages.map((message) => (
             <ChatMessage key={message.id} message={message} />
           ))}
+          {isLoading && lastMessageIsUser && (
+            <ChatMessage
+              message={{ role: "assistant", content: "Thinking..." }}
+            />
+          )}
+          {error && (
+            <ChatMessage
+              message={{
+                role: "assistant",
+                content: "Something went wrong. Please try again.",
+              }}
+            />
+          )}
+          {!error && messages.length === 0 && (
+            <div className="flex h-full items-center justify-center gap-3">
+              <Bot />
+              Ask the AI a question about your notes
+            </div>
+          )}
         </div>
         <form onSubmit={handleSubmit} className="m-3 flex gap-1">
+          <Button
+            title="Clear chat"
+            variant={"outline"}
+            size={"icon"}
+            className="shrink-0"
+            type="button"
+            onClick={() => setMessages([])}
+          >
+            <Trash />
+          </Button>
           <Input
             value={input}
             onChange={handleInputChange}
             placeholder="Say something..."
+            ref={inputRef}
           />
           <Button type="submit" disabled={isLoading}>
             {" "}
@@ -55,11 +107,40 @@ const ChatbotBox: React.FC<Props> = ({ open, onClose }) => {
 
 export default ChatbotBox;
 
-function ChatMessage({ message: { role, content } }: { message: Message }) {
+function ChatMessage({
+  message: { role, content },
+}: {
+  message: Pick<Message, "role" | "content">;
+}) {
+  const { user } = useUser();
+
+  const isAIMessage = role === "assistant";
+
   return (
-    <div className="mb-1">
-      <div className="text-xs">{role}</div>
-      <div className="text-sm">{content}</div>
+    <div
+      className={cn(
+        "mb-3 flex items-center",
+        isAIMessage ? "me-5 justify-start" : "ms-5 justify-end",
+      )}
+    >
+      {isAIMessage && <Bot className="mr-2 shrink-0" />}
+      <p
+        className={cn(
+          "whitespace-pre-line rounded-md border px-3 py-2",
+          isAIMessage ? "bg-background" : "bg-primary text-primary-foreground",
+        )}
+      >
+        {content}
+      </p>
+      {!isAIMessage && user?.imageUrl && (
+        <Image
+          src={user.imageUrl}
+          alt="User image"
+          width={100}
+          height={100}
+          className="ml-2 h-10 w-10 rounded-full object-cover"
+        />
+      )}
     </div>
   );
 }
